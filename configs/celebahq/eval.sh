f84f4593e115aac62c2fe219d305aec4e6cee5a4
YWangatTUD <103425468+YWangatTUD@users.noreply.github.com>
2024-01-25 17:45:31 +0800

    Update eval.sh

--- configs/celebahq/eval.sh
+++ configs/celebahq/eval.sh
@@ -1,5 +1,8 @@
-CUDA_VISIBLE_DEVICES=0 accelerate launch --num_processes=1 src/eval/eval.py \
---ckpt_path /path_to_your_logs/lsd/movi-e/output_norm_linear/checkpoint-xxx/ \
---dataset_root /path_to_your_movi/movi-e/movi-e-val-with-label/images/ \
---dataset_glob '**/*.png' --resolution 256 --linear_prob_train_portion 0.83 \
---enable_xformers_memory_efficient_attention --mixed_precision fp16
+CUDA_VISIBLE_DEVICES=0 accelerate launch --num_processes=1 eval.py \
+--mixed_precision fp16 \
+--seed 42 \
+--batch_size 32 --num_validation_images 32 \
+--output_dir ~/Projects/latent-slot-diffusion-decomp/lsd/celebahq/gen_images \
+--scheduler_config configs/celebahq/scheduler/scheduler_config.json --dataset_root /space/ywang86/celebahq_data128x128/ \
+--dataset_glob '**/*.jpg' --resolution 128 --vit_input_resolution 128 \
+--ckpt_path ~/Projects/latent-decomposed-diffusion/checkpoint-175000
